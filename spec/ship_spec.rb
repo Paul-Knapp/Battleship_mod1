4ff21d828c1bd59c1805b046b1b21c234aa819c8
Paul Knapp <ppankluap@gmail.com>
2024-09-05 16:44:12 -0500

    test: add test for name and length

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,10 +3,23 @@ require './lib/ship'
 
 RSpec.describe Ship do
     before(:each) do
-        @ship1 = Ship.new("Cruiser", 3 )
+        @ship1 = Ship.new('Cruiser', 3 )
     end
 
     it 'exists' do 
         expect(@ship1).to be_an_instance_of (Ship)
+    end
+
+    it 'has attributes' do 
+        it 'has a name' do
+            expect(@ship1.name).to eq ('Cruiser')
+        end
+
+        it 'has a length' do
+            expect(@ship1.length).to eq (3)
+        end
+       
+
+       
     end
 end
