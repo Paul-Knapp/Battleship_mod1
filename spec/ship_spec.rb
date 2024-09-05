a1aa0086f722f296d2a3717128d3108764a013bb
Paul Knapp <ppankluap@gmail.com>
2024-09-05 16:54:59 -0500

    test: test for ship hits and ability to be sunk

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,25 +3,46 @@ require './lib/ship'
 
 RSpec.describe Ship do
     before(:each) do
-        @ship1 = Ship.new('Cruiser', 3 )
+        @cruiser = Ship.new('Cruiser', 3 )
     end
 
     it 'exists' do 
-        expect(@ship1).to be_an_instance_of (Ship)
+        expect(@cruiser).to be_an_instance_of (Ship)
+    
+            it 'has attributes' do 
+                it 'has a name' do
+                    expect(@cruiser.name).to eq ('Cruiser')
+                end
+
+                it 'has a length' do
+                    expect(@cruiser.length).to eq (3)
+                end
+
+                it 'has health' do
+                    expect(@cruiser.health).to eq (3)
+                    expect(@cruiser.health).to eq (@ship.length)
+                end
+
+                it 'isnt sunk' do
+                    expect(@cruiser.sunk).to eq (false)
+                end
+            end
     end
 
-    it 'has attributes' do 
-        it 'has a name' do
-            expect(@ship1.name).to eq ('Cruiser')
-        end
+    it 'can be hit' do
+        @cruiser.hit
+        expect(@cruiser.health).to eq (2)
+    end
 
-        it 'has a length' do
-            expect(@ship1.length).to eq (3)
-        end
-
-        it 'has health' do
-            expect(@ship1.health).to eq (3)
-            expect(@ship1.health).to eq (@ship.length)
-        end
+    it 'can be sunk' do
+        @cruiser.hit
+        expect(@cruiser.health).to eq (2)
+        expect(@cruiser.sunk).to eq (false)
+        @cruiser.hit
+        expect(@cruiser.health).to eq (1)
+        expect(@cruiser.sunk).to eq (false)
+        @cruiser.hit
+        expect(@cruiser.health).to eq (0)
+        expect(@cruiser.sunk).to eq (true)
     end
 end
