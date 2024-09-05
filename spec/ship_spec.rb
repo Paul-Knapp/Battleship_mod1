968e0ec3a2b0354fea67ad978928af2e5c0b70a8
Paul Knapp <ppankluap@gmail.com>
2024-09-05 16:40:55 -0500

    test: add test to see if the class instantiates

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Ship do
     before(:each) do
         @ship1 = Ship.new("Cruiser", 3 )
     end
+
+    it 'exists' do 
+        expect(@ship1).to be_an_instance_of (Ship)
+    end
 end
