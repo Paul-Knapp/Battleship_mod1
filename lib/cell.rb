e3f7672dcc789f1cf210727787baa1041143ad14
Paul Knapp <ppankluap@gmail.com>
2024-09-05 22:11:06 -0500

    feat: add method for firing upon the cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,10 @@
 class Cell
-    attr_reader :coordinate, :ship
+    attr_reader :coordinate, :ship, :fired_upon?
     def initialize(coordinate)
         @coordinate = coordinate
         ships = []
         ship = ships[0]
+        fired_upon? = false
     end
 
     def place_ship(type)
@@ -12,5 +13,9 @@ class Cell
 
     def empty?
         ships.length == 0
+    end
+
+    def fire_upon
+        fired_upon? = true
     end
 end
