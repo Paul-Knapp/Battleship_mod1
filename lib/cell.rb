fa67d45afbb06db307520c56611e2b4b2c938fbe
Paul Knapp <ppankluap@gmail.com>
2024-09-05 22:05:05 -0500

    feat: add place ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,12 @@
 class Cell
+    
     def initialize(coordinate)
         @coordinate = coordinate
         ships = []
         ship = ships[0]
+    end
+
+    def place_ship(type)
+        ships << type
     end
 end
