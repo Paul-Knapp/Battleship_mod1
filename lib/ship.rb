a705a7bc15d4a0603a669bc318fbfa1a3d257bb2
Eric DeShetler <ejdesh@gmail.com>
2024-09-05 18:28:05 -0700

    isn't sunk

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,10 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health, :sunk
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
+    @sunk = false
   end
 end
