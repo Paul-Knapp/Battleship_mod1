354f95511a22a5bdb4420d5548259b232b284270
Eric DeShetler <ejdesh@gmail.com>
2024-09-08 00:50:12 -0700

    refactor: valid_coordinte

--- lib/board.rb
+++ lib/board.rb
@@ -19,11 +19,12 @@ class Board
   end
 
   def valid_coordinate?(input_coordinate)
-    @cells.each do |cell|
-      cell_coordinate = cell[1].coordinate
-      return true if cell_coordinate == input_coordinate
-    end
-    false
+    # @cells.each do |cell|
+    #  cell_coordinate = cell[1].coordinate
+    #  return true if cell_coordinate == input_coordinate
+    # end
+    @cells[input_coordinate] ? true : false
+    # false
   end
 
   def get_columns(coordinates)
