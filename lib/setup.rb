fa1c7b0a40ecd10462cc7505377a053209b5c0bf
Eric DeShetler <ejdesh@gmail.com>
2024-09-09 15:39:12 -0700

    will probably delete this branch2

---
+++ lib/setup.rb
@@ -0,0 +1,33 @@
+require './lib/board'
+require './lib/ship'
+
+class Setup
+  def initialize
+  end
+  # Computer can place ships randomly in valid locations
+
+  # Place 1st ship
+
+  #   create a cruiser
+  def prepare_for_battle(player_type)
+  end
+
+  # c_ship_1 = Ship.new('Cruisin_for_a_Bruisin', 3)
+
+  #   generate a random starting coordinate
+
+  #   generate two remaining coordinates
+
+  #   place cruiser
+
+  # Place 2nd ship
+
+  #   create a Submarine
+
+  #   generate a random starting coordinate
+
+  #   generate two remaining coordinates
+
+  # User can enter valid sequences to place both ships
+  # Entering invalid ship placements prompts user to enter valid placements
+end
