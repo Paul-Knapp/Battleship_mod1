1ea2509b7bb9eac017d722d02a6592ce35885b35
Paul Knapp <ppankluap@gmail.com>
2024-09-09 15:05:45 -0500

    feat: psuedocode for game class

---
+++ lib/game.rb
@@ -0,0 +1,21 @@
+class Game 
+    attr_accessor :board, :turns
+    def initialize(board)
+        @turns = []
+        @board = board
+    end
+
+    #starts at main menu screen 
+        #p to play if p to self.
+        #q to quit
+
+    #let both both player and computer place ships
+
+    #take a player guess
+        #take turn?
+        #tell you to try again if you already guessed the coordinate
+
+    #take a guess from a computer
+
+    #show the board with computer ships hidden
+end
