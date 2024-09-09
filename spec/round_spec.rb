ccf9170d3d7628f27d2d580f808b2bdcb33c6f32
Paul Knapp <ppankluap@gmail.com>
2024-09-09 14:25:45 -0500

    test: add psuedocoded tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 Rspec.describe Round do
+    #it exists
 
+    #it has a board
+
+    #it can take a player turn
+
+    #it can take a computer turn
+
+    # # it knows results
+
+    #it knows that you have already shot at a coordinate
+
+    
 end
