0e9c3e307b20467b3bcfbffd6f8eafaf73e7333e
Paul Knapp <ppankluap@gmail.com>
2024-09-08 22:20:22 -0500

    test: add a before each block to setup tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Turn do
-    
+    before(:each)
+    @turn = Turn.new(guess)
+    @board1 = Board.new
+    @cruiser = Ship.new('Cruiser', 3)
+    @submarine = Ship.new('Submarine', 2)
+    @cell1 = @board1.cells['A1']
+    @cell2 = @board1.cells['A2']    
+    it '#exists' do
+        expect(@turn).to be_an_instance_of(Turn)
+    end
 end
