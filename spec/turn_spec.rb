0ae191ea1b0345ab5dc29995d70d3792110e099e
Paul Knapp <ppankluap@gmail.com>
2024-09-08 22:24:37 -0500

    test: add tests for instantiation

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,13 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Turn do
     before(:each)
-    @turn = Turn.new(guess)
+    @turn = Turn.new('A1')
     @board1 = Board.new
     @cruiser = Ship.new('Cruiser', 3)
     @submarine = Ship.new('Submarine', 2)
     @cell1 = @board1.cells['A1']
     @cell2 = @board1.cells['A2']    
-    it '#exists' do
-        expect(@turn).to be_an_instance_of(Turn)
+
+    describe "#initialize" do
+        it 'exists' do
+            expect(@turn).to be_an_instance_of(Turn)
+        end
+
+        it 'has a guess do' do
+            expect(@turn.guess).to be('A1')
+        end
     end
 end
