73c1097925d35fe4e723a36ff896e66e233a6417
Paul Knapp <ppankluap@gmail.com>
2024-09-09 09:57:01 -0500

    feat: add ability to give feedback to the player

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,20 @@
 class Turn
-    attr_reader :shot
+    attr_reader :shot, :board
 
-    def initialize(shot)
+    def initialize(shot, board)
         @shot = shot
+        @board = board
     end
 
     def is_a_hit
-        
+        @board.cells[shot].ship != nil
     end
 
     def feedback
-
+        if is_a_hit == true
+            "your shot on #{shot} was a hit"
+        elsif is_a_hit = false
+             "your shot on #{shot} was a miss"
+        end
     end
 end
