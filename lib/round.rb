8414883dfc07ba1bd45c896bd0cf957b50691085
Paul Knapp <ppankluap@gmail.com>
2024-09-09 14:25:06 -0500

    feat: add psuedocode to show the potential shape of the program

---
+++ lib/round.rb
@@ -0,0 +1,15 @@
+class Round
+    attr_accessor :board, :turns
+    def initialize(board)
+        @turns = []
+        @board = board
+    end
+
+    #take a player guess
+        #take turn?
+        #tell you to try again if you already guessed the coordinate
+
+    #take a guess from a computer
+
+    #posiibly show the board (although i think this might actually go in the game file)
+end
