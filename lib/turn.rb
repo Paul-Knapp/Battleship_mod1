ac22b94ce7f160e56cecbc243c0d0343db731f1a
Eric DeShetler <ejdesh@gmail.com>
2024-09-09 16:36:53 -0700

    feat: Turn can display boards

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,20 +1,48 @@
 class Turn
-    attr_reader :shot, :board
+  attr_reader :shot, :board
 
-    def initialize(shot, board)
-        @shot = shot
-        @board = board
-    end
+  def initialize(shot, board1, board2)
+    @shot = shot
+    @board1 = board1
+    @board2 = board2
+  end
 
-    def is_a_hit
-        @board.cells[shot].ship != nil
-    end
+  def take_turn
+    # Computer does not fire on the same spot twice
+    @@shots_fired = []
 
-    def feedback
-        if is_a_hit == true
-            "your shot on #{shot} was a hit"
-        elsif is_a_hit = false
-             "your shot on #{shot} was a miss"
-        end
+    # User board is displayed showing hits, misses, sunken ships, and ships
+    @board1.render(true)
+
+    # Computer board is displayed showing hits, misses, and sunken ships
+    @board2.render(true)
+  end
+
+  def random_shot
+    # Computer chooses a random shot
+  end
+
+  def user_shot
+    # User can choose a valid coordinate to fire on
+    # Entering invalid coordinate prompts user to enter valid coordinate
+    # User is informed when they have already fired on a coordinate
+  end
+
+  def damage_report
+    # Both computer and player shots are reported as a hit, sink, or miss
+    # Board is updated after a turn
+  end
+
+  def is_a_hit
+    !@board.cells[shot].ship.nil?
+  end
+
+  def feedback
+    if is_a_hit
+      "your shot on #{shot} was a hit"
+    elsif is_a_hit
+      "your shot on #{shot} was a miss"
     end
+  end
 end
+
