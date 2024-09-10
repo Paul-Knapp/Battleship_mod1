867e39f75850d1b8cc64d35472039c918bf84d72
Eric DeShetler <ejdesh@gmail.com>
2024-09-09 17:58:31 -0700

    feat: computer_shot

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,12 @@
 class Turn
   attr_reader :shot, :board
 
+  @@shots_fired = []
+
+  def self.shots_fired
+    @@shots_fired
+  end
+
   def initialize(shot, board1, board2)
     @shot = shot
     @board1 = board1 # User
@@ -8,9 +14,6 @@ class Turn
   end
 
   def take_turn
-    # Computer does not fire on the same spot twice
-    @@shots_fired = []
-
     # User board is displayed showing hits, misses, sunken ships, and ships
     @board1.render(true)
 
@@ -19,7 +22,16 @@ class Turn
   end
 
   def random_shot
-    # Computer chooses a random shot
+    (@board1.cells.keys - @@shots_fired).sample
+  end
+
+  def computer_shot
+    # Computer chooses a random shot - done
+    # Computer does not fire on the same spot twice - done
+    target = random_shot
+    @board1.cells[target].fire_upon
+    @@shots_fired << target
+    target
   end
 
   def user_shot
@@ -29,6 +41,14 @@ class Turn
   end
 
   def damage_report
+    all_targets = board1.place(p1s1, %w[A1 A2 A3])
+    board1.place(p1s2, %w[B1 B2])
+    board2.place(p2s1, %w[A1 A2 A3])
+    board2.place(p2s2, %w[D1 D2])
+
+    turn = Turn.new('A1', board1, board2)
+    turn.take_turn
+
     # Both computer and player shots are reported as a hit, sink, or miss
     # Board is updated after a turn
   end
