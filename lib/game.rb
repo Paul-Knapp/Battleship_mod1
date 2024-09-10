9bc8d82dc5785f97c6021f61fb7afcbed6e8e592
Paul Knapp <ppankluap@gmail.com>
2024-09-09 22:37:13 -0500

    feat: add tested method for placing ships randomly

--- lib/game.rb
+++ lib/game.rb
@@ -4,42 +4,72 @@ class Game
         @turns = []
         @cruiser = Ship.new('Cruiser', 3)
         @submarine = Ship.new('Submarine', 2)
+        turn = Turn.new
     end
 
     def start
-        puts "Welcome to BATTLESHIP\n
-            Enter p to play. Enter q to quit."
-        input = gets
-        if input == ('q')
-            self.place_ships
-        elsif input == ('p')
+        puts "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
+        input = gets.chomp.downcase
+        if input == 'p'
+            place_ships
+        elsif input == 'q'
+            p 'Too bad'
         end
     end
         
     def place_ships
-        #computer places ships
-        p "I have laid out my ships on the grid.\n
-            You now need to lay out your two ships.\n
-The Cruiser is three units long and the Submarine is two units long.\n
-        +#{@player_board.render}
+        place_ships_randomly(@cruiser)
+        place_ships_randomly(@submarine)
+        p "I have laid out my ships on the grid.\n" +
+            "You now need to lay out your two ships.\n" +
+"The Cruiser is three units long and the Submarine is two units long.\n"
+        +{@turns[0].render}
         Enter the squares for the Cruiser (3 spaces):"
-        @player_board.place_ship(cruiser[gets])
+        @turns[0].board2.place_ship(cruiser, [gets.chomp.split])
         p "Enter the squares for the Submarine (2 spaces):"
        until @player_board.place_ship(submarine[gets]) == true 
-        @player_board.place_ship(submarine[gets]) == true
+        @player_board.place_ship(submarine[gets.chomp.split]) == true
        else
        end
     end
-    
-    def 
 
-    end
+    def generate_coordinates(start_coordinate, length, orientation)
+        row = start_coordinate[0]
+        col = start_coordinate[1..-1].to_i
         
-    def end_game?
-        if @turn.board1.cells[1].each do |cell|
-            if cell 
+        if orientation == :horizontal
+          (col...(col + length)).map { |c| "#{row}#{c}" }
+        else
+          (row.ord...(row.ord + length)).map { |r| "#{r.chr}#{col}" }
+        end
+      end
+    
+    def place_ships_randomly(ship)
+        turns[0].board2.columns_array = verticle
+        turns[0].board2.rows_array = horizontal(ship)
+            attempts = 0
+            max_attempts = 100
+          
+            while attempts < max_attempts
+              start_row = rows_array.sample
+              start_col = columns_array.sample
+              start_coordinate = "#{start_row}#{start_col}"
+          
+              orientation = [:horizontal, :vertical].sample
+          
+              coordinates = generate_coordinates(start_coordinate, ship.length, orientation)
+          
+              if valid_placement?(ship, coordinates)
+                place(ship, coordinates)
+                return true
+              end
+              attempts += 1
+            end
+            false 
         end
     end
+        
+    
 
     #take a guess from a computer
 
