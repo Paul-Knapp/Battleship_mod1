277ac0622b99732fba74a6ed8218b9ef0b39c5f3
Paul Knapp <ppankluap@gmail.com>
2024-09-11 15:52:31 -0500

    test:remove unused code

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Game do
   end
 
   it '#exists' do
-    expect(game).to be_an_instance_of(Game)
+    expect(@game).to be_an_instance_of(Game)
   end
 
   it '#has both boards' do
@@ -14,29 +14,4 @@ RSpec.describe Game do
     expect(@game.computer_board).to be_an_instance_of(Board)
   end
 
-  xit '#can start a new game' do
-    # expect{game.start}.to output(Welcome to BATTLESHIP
-    # Enter p to play. Enter q to quit.).to_stdout
-  end
-
-  # # it knows results
-
-  # it knows that you have already shot at a coordinate
-
-  # it displays results with every shot taken
-
-  it '#can end the game' do
-    it 'ends the game when you lose' do
-    end
-
-    it 'ends the game when you win' do
-    end
-
-    it 'reports a winner' do
-    end
-
-    it 'returns to main menu' do
-    end
-  end
 end
-
