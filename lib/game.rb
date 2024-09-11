1dac942fcd9d787c51c258d7a2ff8488c0a851e2
Paul Knapp <ppankluap@gmail.com>
2024-09-10 20:18:56 -0500

    feat:place player ships method split with a helper method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game 
-    attr_accessor :turns, :player_board, :computer_board
+    attr_accessor :turns, :player_board, :computer_board, :cells
     def initialize
         @cruiser = Ship.new('Cruiser', 3)
         @submarine = Ship.new('Submarine', 2)
@@ -8,6 +8,32 @@ class Game
         @player_board = Board.new
         @computer_board = Board.new
         @turn = Turn.new(@player_board ,@computer_board)
+    end
+
+    def place_ships
+        place_ships_randomly(@c_cruiser)
+        place_ships_randomly(@c_submarine)
+        p "I have laid out my ships on the grid.\n" +
+            "You now need to lay out your two ships.\n" +
+"The Cruiser is three units long and the Submarine is two units long.\n"
+
+        place_player_ships('Cruiser', 3)
+        place_player_ships('Submarine', 2)
+       
+        play_the_game
+    end
+    
+    def place_player_ship(ship, length)
+        loop do
+          puts "Enter the squares for the #{ship.name} (#{length} spaces):"
+          coordinates = gets.chomp.split
+          if @player_board.place(ship, coordinates)
+            puts "#{ship.name} placed"
+            return
+          else
+            puts "Invalid placement try again"
+          end
+        end
     end
 
     def start
@@ -20,22 +46,6 @@ class Game
         end
     end
         
-    def place_ships
-        place_ships_randomly(@c_cruiser)
-        place_ships_randomly(@c_submarine)
-        p "I have laid out my ships on the grid.\n" +
-            "You now need to lay out your two ships.\n" +
-"The Cruiser is three units long and the Submarine is two units long.\n"
-        +{@turns[0].render}
-         p "Enter the squares for the Cruiser (3 spaces):"
-        @player_board.place_ship(@cruiser, [gets.chomp.split])
-        p "Enter the squares for the Submarine (2 spaces):"
-       until @player_board.place_ship(submarine[gets]) == true 
-        @player_board.place_ship(@submarine[gets.chomp.split]) == true
-        play_the_game
-       else
-       end
-    end
 
     def generate_coordinates(start_coordinate, length, orientation)
         row = start_coordinate[0]
@@ -46,18 +56,18 @@ class Game
         else
           (row.ord...(row.ord + length)).map { |r| "#{r.chr}#{col}" }
         end
-      end
+    end
     
     def place_ships_randomly(ship)
-        @computer_board.columns_array = verticle
-        @computer_board.rows_array = horizontal(ship)
+        vertical = @computer_board.columns_array 
+        horizontal = @computer_board.rows_array
             max_attempts = 150 
             attempts = 0
            
           
             while attempts < max_attempts
-              start_row = rows_array.sample
-              start_col = columns_array.sample
+              start_row = @computer_board.rows_array.sample
+              start_col = @computer_board.columns_array.sample
               start_coordinate = "#{start_row}#{start_col}"
           
               orientation = [:horizontal, :vertical].sample
@@ -65,21 +75,22 @@ class Game
               coordinates = generate_coordinates(start_coordinate, ship.length, orientation)
           
               if @computer_board.valid_placement?(ship, coordinates)
-                place(ship, coordinates)
+                @computer_board.place(ship, coordinates)
                 return true
               end
               attempts += 1
             end
-            false 
-        end
+
+            false
     end
         
     
     def play_the_game
         @turn.take_turn until
-        {@c_cruiser.sunk? && @c_submarine.sunk?} ||{@cruiser.sunk? && @submarine.sunk?}
+        (@c_cruiser.sunk? && @c_submarine.sunk?) ||(@cruiser.sunk? && @submarine.sunk?)
         p "press enter to continue"
         gets
         game.start
     end
+    
 end
